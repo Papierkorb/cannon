f7a121468b59686428beac734e9bbb5224d36bed
Stefan Merettig <stefan-merettig@nuriaproject.org>
2017-07-25 00:16:30 +0200

    spec/spec_helper.cr: Add more in-source docs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -8,7 +8,7 @@ require "./rpc/service_fixtures"
 ## Helper methods
 
 # If you're looking at the specs to learn how to use this library, use this
-# method for inspiration.  It uses always the fastest solution, and gracefully
+# method for inspiration.  It always uses the fastest solution, and gracefully
 # falls back to slower implementations without any run-time penality.
 def en_decode(value, encoded_size)
   io = IO::Memory.new
@@ -20,6 +20,9 @@ def en_decode(value, encoded_size)
   Cannon.decode io, typeof(value)
 end
 
+# This version uses the slow path.  Slow is relative, really: It's still blazing
+# fast .. just not that fast anymore.  It's more stable though, and if you
+# prefer the more OOP-y approach, there's nothing wrong with this!
 def slow_en_decode(value, encoded_size)
   io = IO::Memory.new
   value.to_cannon_io io
